efaad0afff43de3c557b1127d636cc9a8b0e7cc0
Pinku <coderpinku@gmail.com>
2023-01-14 08:10:54 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+        
+        <h2>Meme Generator</h2>
+        <h4>React course - Project-4 </h4>    
+    </header>
+    
+  )
+}
+
+export default Header
